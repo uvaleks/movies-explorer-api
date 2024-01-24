1ea44672c358f9bb776604b12371dcdef38c0ecd
uvaleks <uvaleks@protonmail.com>
2024-01-24 21:30:28 +0300

    hotfix: env

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const router = require('./routes');
 if (process.env.NODE_ENV === 'production') {
   dotenv.config({ path: '.env.prod' });
 } else {
-  dotenv.config({ path: '.env' });
+  dotenv.config({ path: '.env.dev' });
 }
 
 const app = express();
